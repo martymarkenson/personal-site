04af3ea588eb533a1718fb052376a4815b56e32d
mcflycs <marty.markenson@codingscape.com>
2025-06-28 22:03:22 -0400

    fixed a couple more build

--- src/pages/thank-you.jsx
+++ src/pages/thank-you.jsx
@@ -9,12 +9,12 @@ export default function ThankYou() {
         <title>Message sent - Marty Markenson</title>
         <meta
           name="description"
-          content="Thanks for reaching out. I'll get back to you soon."
+          content="Thanks for reaching out. I&apos;ll get back to you soon."
         />
       </Head>
       <SimpleLayout
         title="Thanks for your message."
-        intro="I've received your message and will get back to you within 24 hours. I'm looking forward to discussing your project and how I can help bring your ideas to life."
+        intro="I&apos;ve received your message and will get back to you within 24 hours. I&apos;m looking forward to discussing your project and how I can help bring your ideas to life."
       />
     </>
   )
